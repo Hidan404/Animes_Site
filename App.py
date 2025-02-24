0bd0601e052041e847209e104ba17948b3407afc
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-02-24 12:12:31 -0300

    refactor anime display logic and improve UI for logged-in users

--- App.py
+++ App.py
@@ -30,26 +30,23 @@ elif choice == "Registrar":
         st.success("Usuário criado! Faça login.")
 
 elif choice == "Animes":
-    st.subheader("📺 Lista de Animes")
+    
     if "logged_in" in st.session_state:
-        from Api import get_animes
+        st.markdown("<h1 style='text-align: center; font-family: Cursive; margin-bottom: 1rem'>📺 Animes da Temporada</h1>", unsafe_allow_html=True)
+
         animes = get_animes()
-        for anime in animes:
-            st.write(f"🎞️ {anime['title']}")
+        if len(animes) > 0:
+            for anime in animes:
+                col1, col2 = st.columns([1, 3])
+            with col1:
+                st.image(anime["images"]["jpg"]["image_url"], width=150)
+            with col2:
+                st.subheader(anime["title"])
+                st.write(anime["synopsis"][:200] + "...")
+                st.write(f"📅 Estreia: {anime['aired']['from'][0:10]}")
+                st.write(f"⭐ Nota: {anime['score']}")
+                st.link_button("Mais detalhes", anime["url"])
     else:
         st.warning("Faça login para ver os animes!")
 
 
-st.markdown("<h1 style='text-align: center; font-family: Cursive; margin-bottom: 4rem'>📺 Animes da Temporada</h1>", unsafe_allow_html=True)
-
-animes = get_animes()
-for anime in animes:
-    col1, col2 = st.columns([1, 3])
-    with col1:
-        st.image(anime["images"]["jpg"]["image_url"], width=150)
-    with col2:
-        st.subheader(anime["title"])
-        st.write(anime["synopsis"][:200] + "...")
-        st.write(f"📅 Estreia: {anime['aired']['from'][0:10]}")
-        st.write(f"⭐ Nota: {anime['score']}")
-        st.link_button("Mais detalhes", anime["url"])
