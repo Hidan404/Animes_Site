e421962a830c93e5112640c2f1b09690748e15e6
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-02-24 12:30:49 -0300

    add debug print statement to display API response for anime data

--- Api.py
+++ Api.py
@@ -5,6 +5,7 @@ api_url = "https://api.jikan.moe/v4/seasons/now"
 def get_animes(api = api_url):
     resposta = requests.get(api)
     if resposta.status_code == 200:
+        print(resposta.json())
         return resposta.json().get("data", [])
     return []
 
